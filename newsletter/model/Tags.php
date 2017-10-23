61684eb2c9acd6218328d4ed2c4c589b014cee1f
Smasty <martin@smasty.net>
2017-10-24 00:37:26 +0200

    add Tags model

---
+++ newsletter/model/Tags.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Newsletter\Model;
+
+
+class Tags extends BaseModel{
+    const TABLE = 'tags';
+    const MAPPING_TABLE = 'question_tags';
+
+    public function getQuestionTags($question_id){
+        return $this->db->select('t.id, t.name', static::MAPPING_TABLE)
+            ->leftJoin(static::TABLE. 't', 't.id = ' . static::MAPPING_TABLE . '.tag_id')
+            ->where('question_id', $question_id);
+    }
+}
