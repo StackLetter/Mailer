5d6fb5bc7ebcc20e218f4438e36ad9cab3b10fb0
Smasty <martin@smasty.net>
2017-10-31 17:07:38 +0100

    add PID file

--- bin/mailer.php
+++ bin/mailer.php
@@ -4,6 +4,9 @@
 /** @var Nette\DI\Container $container */
 $container = require_once __DIR__ . '/../bootstrap.php';
 
+// Create PID file
+file_put_contents($container->parameters['tempDir'] . '/mailer.pid', getmypid() . "\n");
+
 /** @var Newsletter\Mailer $mailer */
 $mailer = $container->getByType(Newsletter\Mailer::class);
 
