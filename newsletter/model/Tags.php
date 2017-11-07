54016bfaa19eba988c4cd54e13a76c1ce714e495
Smasty <martin@smasty.net>
2017-11-07 17:55:56 +0100

    unique tags fix for bad import

--- newsletter/model/Tags.php
+++ newsletter/model/Tags.php
@@ -8,8 +8,8 @@ class Tags extends BaseModel{
     const MAPPING_TABLE = 'question_tags';
 
     public function getQuestionTags($question_id){
-        return $this->db->select('t.id, t.name', static::MAPPING_TABLE)
+        return $this->db->select('MAX(t.id) AS id, t.name', static::MAPPING_TABLE)
             ->leftJoin(static::TABLE. ' t', 't.id = ' . static::MAPPING_TABLE . '.tag_id')
-            ->where('question_id', $question_id);
+            ->where('question_id', $question_id)->group('t.name');
     }
 }
